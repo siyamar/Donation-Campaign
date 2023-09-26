0fc2d0c60ff4d9e13a9b21fe3a2f091a32ee6e5b
siyamar <ab.rahmansiyam@gmail.com>
2023-09-26 11:47:27 +0600

    Added root and home component

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './Components/Pages/Home/Home';
+import Root from './Components/Root/Root';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
